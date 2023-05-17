8918452037db565cc065224e51f2bd0cbce4d097
Phil <pk.oaferina@gmail.com>
2023-05-18 00:07:35 +0400

    fix: footer links fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,48 @@
-import logo from "../assets/chillflix-logo.png"
+import { Link } from "react-router-dom";
+import logo from "../assets/chillflix-logo.png";
 
-
-function Footer(){
-	return (
-		<footer>
-			<div className="row">
-				<div className="footer__container">
-					<div className="footer__logo--container">
-						<img src={logo} alt="chillflix" className="footer__logo" />
-						<div className="footer__copyright">Copyright &copy; 2023 ChillFlix</div>
-					</div>
-					<div className="footer__links">
-						<div className="footer__link">Browse</div>
-						<div className="footer__link--divider"></div>
-						<div className="footer__link">API</div>
-						<div className="footer__link--divider"></div>
-						<div className="footer__link">Github</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-	)
+function Footer() {
+  return (
+    <footer>
+      <div className="row">
+        <div className="footer__container">
+          <div className="footer__logo--container">
+            <img src={logo} alt="chillflix" className="footer__logo" />
+            <div className="footer__copyright">
+              Copyright &copy; 2023 ChillFlix
+            </div>
+          </div>
+          <div className="footer__links">
+            <div className="footer__link">
+              <Link to={"/browse"} className="nav__link">
+                Browse
+              </Link>
+            </div>
+            <div className="footer__link--divider"></div>
+            <div className="footer__link">
+              <a
+                href="https://www.omdbapi.com/"
+                target="_blank"
+                className="nav__link"
+              >
+                API
+              </a>
+            </div>
+            <div className="footer__link--divider"></div>
+            <div className="footer__link">
+              <a
+                href="https://www.omdbapi.com/"
+                target="_blank"
+                className="nav__link"
+              >
+                GitHub
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
