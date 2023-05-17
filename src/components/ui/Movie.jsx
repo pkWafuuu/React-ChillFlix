c86ce7d82d802e8c1ec65bc6178650dceeb8f776
Phil <pk.oaferina@gmail.com>
2023-05-18 02:18:31 +0400

    feat: add breakpoints 768px and 576px

--- src/components/ui/Movie.jsx
+++ src/components/ui/Movie.jsx
@@ -13,38 +13,40 @@ function Movie({ movie }) {
         `https://www.omdbapi.com/?apikey=f5504bbb&i=${movie.imdbID}&plot=${movie.Type}`
       );
       setMovieInfo(data);
-			setTimeout(() => {
-				setLoading(false);
-			}, 1000);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
     }
     fetchMovieInfo();
   }, []);
 
   return (
     <>
-      {loading ? (
-        <div className="movie__skeleton">
-					<div className="skeleton movie__img--skeleton"></div>
-					<div className="skeleton movie__title--skeleton"></div>
-					<div className="skeleton movie__year--skeleton"></div>
-				</div>
-      ) : (
-        <div className="movie">
-          <div className="movie__wrapper">
-            <img src={movie.Poster} alt="No Poster" className="movie__img" />
-            <div className="movie__info--container">
-							<FontAwesomeIcon icon="star" className="movie__rating--icon" />
-              <div className="movie__rating">{movieInfo.imdbRating}</div>
-              <div className="movie__genre">{movieInfo.Genre}</div>
-              <Link to={`/browse/${movieInfo.imdbID}`}>
-                <button className="movie__btn">View Info</button>
-              </Link>
+      <div className="movie">
+        {loading ? (
+          <>
+            <div className="skeleton movie__img--skeleton"></div>
+            <div className="skeleton movie__title--skeleton"></div>
+            <div className="skeleton movie__year--skeleton"></div>
+          </>
+        ) : (
+          <>
+            <div className="movie__wrapper">
+              <img src={movie.Poster} alt="No Poster" className="movie__img" />
+              <div className="movie__info--container">
+                <FontAwesomeIcon icon="star" className="movie__rating--icon" />
+                <div className="movie__rating">{movieInfo.imdbRating}</div>
+                <div className="movie__genre">{movieInfo.Genre}</div>
+                <Link to={`/browse/${movieInfo.imdbID}`}>
+                  <button className="movie__btn">View Info</button>
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="movie__title">{movie.Title}</div>
-          <div className="movie__year">{movie.Year}</div>
-        </div>
-      )}
+            <div className="movie__title">{movie.Title}</div>
+            <div className="movie__year">{movie.Year}</div>
+          </>
+        )}
+      </div>
     </>
   );
 }
